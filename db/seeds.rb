41b3e5dad519da47b0f727d880a68d603a5cce9b
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-19 20:34:55 +0200

    using TMDB API to seed movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create!(title: "Wonder Woman 1984",
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
-  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-  rating: 6.9)
+# Movie.create!(title: "Wonder Woman 1984",
+#   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+#   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+#   rating: 6.9)
 
-Movie.create!(title: "The Shawshank Redemption",
- overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
- poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
- rating: 8.7)
+# Movie.create!(title: "The Shawshank Redemption",
+#  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+#  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+#  rating: 8.7)
 
-Movie.create!(title: "Titanic",
-  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
-  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-  rating: 7.9)
+# Movie.create!(title: "Titanic",
+#   overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+#   poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+#   rating: 7.9)
 
-Movie.create!(title: "Ocean's Eight",
-  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
-  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-  rating: 7.0)
+# Movie.create!(title: "Ocean's Eight",
+#   overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+#   poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+#   rating: 7.0)
+
+  # url = "http://tmdb.lewagon.com/movie/top_rated"
+
+
+  movie_data = JSON.parse(URI.open('https://tmdb.lewagon.com/movie/top_rated').read)
+  puts 'Creating movies...'
+
+  # puts movie_data["results"].count
+  movie_data['results'].each do |movie|
+    poster_path = movie['poster_path']
+    Movie.create!(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "https://image.tmdb.org/t/p/original#{poster_path}",
+      rating: movie['vote_average']
+    )
+  end
+
+  puts 'done'
+
+  # the API would say: use https://api.themoviedb.org/3/movie/top_rated?api_key=<your_api_key>
+  # What you should do is replace this part of the url https://api.themoviedb.org/3 by http://tmdb.lewagon.com
