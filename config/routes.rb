6cfd572fe30df57f5975ebea8be2c9759a33c736
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-20 23:34:14 +0200

    Added root path, added styling to list page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # A user can create a new list
   # GET "lists/new"
   # POST "lists"
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
