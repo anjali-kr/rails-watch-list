a479da89255a771a4e8af944ff1505b21f6ba423
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-21 20:29:19 +0200

    add a picture to the List model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
   # A list must have a unique name.
   validates :name, uniqueness: true, presence: true
 
